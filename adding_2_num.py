d6592ad7c40be5c21fae9ffe7241a0c1c1506a29
lavanyagowdam983 <51438513+lavanyagowdam983@users.noreply.github.com>
2019-06-18 10:06:36 +0530

    Update and rename README.md to adding_2_num.py

---
+++ adding_2_num.py
@@ -0,0 +1,19 @@
+def input1(a,b):
+    a=int(input("enter the first number:"))
+    b=int(input("enter the second number:"))
+    return a,b
+
+def add(a,b):
+    sum=a+b
+    return sum
+
+def output(a,b,sum):
+    print("sum of %d and %d is %d",a,b,sum)
+    
+def main():
+ n1,n2=0,0
+ input1(n1,n2)
+ res=add(x,y)
+ output(x,y,res)
+
+ main()
